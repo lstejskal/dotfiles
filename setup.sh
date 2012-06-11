06dbfefe07fa7007d79be0a419c2da70b5f031ad
Lukas Stejskal <lucastej@gmail.com>
2012-06-11 15:27:25 +0200

    Added -f and -h command-line options

--- setup.sh
+++ setup.sh
@@ -1,12 +1,39 @@
+#!/bin/bash
 
-for dotfile in "aliases" "irbrc"
-do
-  echo -n "* \"~/.$dotfile\" "
+# TODO
+# * deal with multiple parameters (convert params into array)
 
-  if [ ! -f ~/.$dotfile ]; then
-    cp ./$dotfile ~/.$dotfile
-    echo "file created"
-  else
-    echo "file already exists!"
-  fi
-done
+# use -f or --force parameter to override configuration files
+# configuration files are not overrided by default
+if [ $1 == "-f" ] || [ $1 == "--force" ]; then
+  OVERRIDE=true
+else
+  OVERRIDE=false
+fi
+
+# use -h or --help parameter to write basic usage info
+if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+  echo "
+This script install dotfiles to your home directory.
+
+Command-line params:
+-h|--help   - prints basic usage info
+-f|--force  - overrides configuration files in you home directory
+            with dotfiles
+"
+
+else
+
+  for dotfile in "aliases" "irbrc"
+  do
+    echo -n "* \"~/.$dotfile\" "
+  
+    if [ ! -f ~/.$dotfile ] || [ OVERRIDE == true ]; then
+      cp ./$dotfile ~/.$dotfile
+      echo "file created"
+    else
+      echo "file already exists!"
+    fi
+  done
+
+fi
