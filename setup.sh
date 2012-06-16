41df5c5875d93d53a37800cc8c7d29701a8053d8
Lukas Stejskal <lucastej@gmail.com>
2012-06-16 18:47:58 +0200

    [FIX] if statements in setup.rb

--- setup.sh
+++ setup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# TODO
-# * deal with multiple parameters (convert params into array)
-
 # use -f or --force parameter to override configuration files
 # configuration files are not overrided by default
-if [ $1 == "-f" ] || [ $1 == "--force" ]; then
+if [ "$1" == "-f" ] || [ "$1" == "--force" ]; then
   OVERRIDE=true
 else
   OVERRIDE=false
 fi
 
 # use -h or --help parameter to write basic usage info
-if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
   echo "
 This script install dotfiles to your home directory.
 
@@ -28,7 +25,8 @@ else
   do
     echo -n "* \"~/.$dotfile\" "
   
-    if [ ! -f ~/.$dotfile ] || [ OVERRIDE == true ]; then
+    if [ ! -f "~/.$dotfile" ] || [ OVERRIDE == true ]; then
+      rm ~/.$dotfile
       cp ./$dotfile ~/.$dotfile
       echo "file created"
     else
